189303cac3578d6f9a4f789aea3075c36a09fef3
Enzo.C <Enzocapi@hotmail.com>
2023-01-04 10:12:51 +0100

    removing some picture

--- pages/galerie.js
+++ pages/galerie.js
@@ -3,11 +3,11 @@ import content from "../content/galerie.md"
 import Layout from "../components/Layout/Layout"
 import Gallery from "../components/Galerie/Gallery"
 
-const { attributes: { titre, photos, corps }} = content
+const { attributes: { titre, photos, corps } } = content
 
 const GalerieView = props => (
-  <Layout>
-    <Gallery photos={photos} titre={titre} corps={corps}/>
-  </Layout>)
+    <Layout>
+        <Gallery photos={photos} titre={titre} corps={corps} />
+    </Layout>)
 
 export default GalerieView
